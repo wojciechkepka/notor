424b2ae9185b9bcaca3cde38c5284709fdb18ef7
wojciechkepka <wojtek.kepka@protonmail.com>
2021-02-13 16:43:09 +0100

    Add a common error box, refactor js

--- static/js/glue.js
+++ static/js/glue.js
@@ -1,8 +1,36 @@
+function request(method, ep, body = null, json = false) {
+    var b = null;
+    var h = {};
+    if (body !== null) {
+        if (json) {
+            b = JSON.stringify(body);
+            h["Content-Type"] = "application/json";
+        } else {
+            b = body;
+        }
+    }
+
+    return fetch(ep, {
+        method: method,
+        body: b,
+        headers: h,
+    });
+}
+
+async function displayErrOrReload(response) {
+    if (response.status !== 200) {
+        const resp = await response.json();
+        var errBox = document.getElementById("errBox");
+        errBox.style.visibility = "visible";
+        errBox.innerText = resp.message;
+    } else {
+        location.reload();
+    }
+}
 
 async function deleteNote(id) {
-    const response = await fetch('/notes/' + id, {
-        method: 'DELETE',
-    });
+    const response = await request("DELETE", "/notes/" + id);
+    await displayErrOrReload(response);
 }
 
 async function addNewNote(event) {
@@ -11,32 +39,18 @@ async function addNewNote(event) {
     const data = new FormData(event.target);
 
     note = {
-        title: data.get('title'),
-        content: data.get('content'),
+        title: data.get("title"),
+        content: data.get("content"),
     };
 
-    const response = await fetch('/notes', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(note),
-    });
-
-    if (response.status !== 200) {
-        const resp = await response.json();
-        var errBox = document.getElementById("newNoteErr");
-        errBox.style.visibility = 'visible';
-        errBox.innerText = resp.message;
-    } else {
-        location.reload();
-    }
+    const response = await request("PUT", "/notes", body = note, json = true);
+    await displayErrOrReload(response);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     var newNote = document.querySelector("#newNote");
     newNote.addEventListener("submit", addNewNote)
 
-    document.getElementById("newNoteErr").style.visibility = "hidden";
+    document.getElementById("errBox").style.visibility = "hidden";
 });
 
