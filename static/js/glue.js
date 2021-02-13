a7ad4fa507621d9e51f5b5ca0984987936edd958
wojciechkepka <wojtek.kepka@protonmail.com>
2021-02-13 16:22:54 +0100

    Update notes javascript

--- static/js/glue.js
+++ static/js/glue.js
@@ -1,6 +1,42 @@
 
-function delete_note(id) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('DELETE', '/notes/' + id, true);
-    xhttp.send();
+async function deleteNote(id) {
+    const response = await fetch('/notes/' + id, {
+        method: 'DELETE',
+    });
 }
+
+async function addNewNote(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    note = {
+        title: data.get('title'),
+        content: data.get('content'),
+    };
+
+    const response = await fetch('/notes', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+    });
+
+    if (response.status !== 200) {
+        const resp = await response.json();
+        var errBox = document.getElementById("newNoteErr");
+        errBox.style.visibility = 'visible';
+        errBox.innerText = resp.message;
+    } else {
+        location.reload();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var newNote = document.querySelector("#newNote");
+    newNote.addEventListener("submit", addNewNote)
+
+    document.getElementById("newNoteErr").style.visibility = "hidden";
+});
+
