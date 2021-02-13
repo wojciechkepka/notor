ad18353a245c99c1e46ecf35f2f976abe19e44ee
wojciechkepka <wojtek.kepka@protonmail.com>
2021-02-13 18:47:40 +0100

    Simplify HTML creation

--- src/handlers/web.rs
+++ src/handlers/web.rs
@@ -2,6 +2,26 @@ use super::*;
 use crate::html::HtmlContext;
 use crate::models::Note;
 use crate::web::{Index, NoteView, INDEX_SCRIPT, INDEX_STYLE};
+use sailfish::TemplateOnce;
+
+fn html_from<B, T, S>(body: B, title: T, script: S, style: S) -> Result<String, Rejection>
+where
+    B: TemplateOnce + Default,
+    T: AsRef<str>,
+    S: AsRef<[u8]>,
+{
+    HtmlContext::builder()
+        .lang("en")
+        .title(format!("Notor - {}", title.as_ref()))
+        .add_meta("viewport", "width=device-width, initial-scale=1")
+        .add_script(std::str::from_utf8(script.as_ref()).map_err(InternalError::reject)?)
+        .add_style(std::str::from_utf8(style.as_ref()).map_err(InternalError::reject)?)
+        .body(body)
+        .build()
+        .map_err(InternalError::reject)?
+        .as_html()
+        .map_err(InternalError::reject)
+}
 
 pub(crate) async fn get_web(conn: Db) -> Result<impl Reply, Rejection> {
     use schema::notes::dsl::*;
@@ -11,17 +31,7 @@ pub(crate) async fn get_web(conn: Db) -> Result<impl Reply, Rejection> {
 
     let body = Index::new(_notes);
 
-    let html = HtmlContext::builder()
-        .lang("en")
-        .title("Notor - index")
-        .add_meta("viewport", "width=device-width, initial-scale=1")
-        .add_script(std::str::from_utf8(INDEX_SCRIPT).map_err(InternalError::reject)?)
-        .add_style(std::str::from_utf8(INDEX_STYLE).map_err(InternalError::reject)?)
-        .body(body)
-        .build()
-        .map_err(InternalError::reject)?
-        .as_html()
-        .map_err(InternalError::reject)?;
+    let html = html_from(body, "index", INDEX_SCRIPT, INDEX_STYLE)?;
 
     Ok(reply::html(html))
 }
@@ -35,20 +45,10 @@ pub(crate) async fn get_web_note(id: i32, conn: Db) -> Result<impl Reply, Reject
         .first::<Note>(&*conn)
         .map_err(|_| NotFound::reject())?;
 
-    let page_title = format!("Notor - `{}`", &note.title);
+    let page_title = note.title.clone();
     let view = NoteView::new(note);
 
-    let html = HtmlContext::builder()
-        .lang("en")
-        .title(page_title)
-        .add_meta("viewport", "width=device-width, initial-scale=1")
-        .add_script(std::str::from_utf8(INDEX_SCRIPT).map_err(InternalError::reject)?)
-        .add_style(std::str::from_utf8(INDEX_STYLE).map_err(InternalError::reject)?)
-        .body(view)
-        .build()
-        .map_err(InternalError::reject)?
-        .as_html()
-        .map_err(InternalError::reject)?;
+    let html = html_from(view, page_title, INDEX_SCRIPT, INDEX_STYLE)?;
 
     Ok(reply::html(html))
 }
